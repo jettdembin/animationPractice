6ef7e642c8d4b50c4fc1f646d502cf4224ef025e
Jett Dembin <71148367+jettdembin@users.noreply.github.com>
2021-09-01 07:35:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,4 @@ tl.to('.slider', { y: "-100%", duration: 1.5, delay: 0.5 });
 tl.to('.intro', { y: "-100%", duration: 1 }, "-=1.025");
 tl.fromTo('nav', { opacity: 0}, { opacity: 1, duration: 1.25 });
 tl.fromTo('.big-text', { opacity: 0}, { opacity: 1, duration: 2.5 }, "-=1");
+
